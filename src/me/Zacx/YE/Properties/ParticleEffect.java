c0fe33b351aa29c2aac5c2a30c4fe2f0cc5eafd9
Zach Dev <zacxdev@gmail.com>
2016-11-11 16:44:45 -0600

    Home - Features

--- src/me/Zacx/YE/Properties/ParticleEffect.java
+++ src/me/Zacx/YE/Properties/ParticleEffect.java
@@ -1,11 +1,18 @@
 package me.Zacx.YE.Properties;
 
+import org.bukkit.Effect;
 import org.bukkit.entity.Player;
 
 public enum ParticleEffect {
 
-	holder;
+	holder(), HEARTS();
 	
-	public void play(Player p) {}
+	public void play(Player p) {
+		
+		if (this == HEARTS) {
+			p.getWorld().playEffect(p.getLocation(), Effect.HEART, 1);
+		}
+		
+	}
 	
 }
